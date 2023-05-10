c78be302f6ce49aabdcd0da1d2e91da635563d6a
David Allbritton <dallbrit@depaul.edu>
2023-05-09 23:08:33 -0700

    Updated the "Explanation" panel with some additional text and links.

--- HelperFunctions.R
+++ HelperFunctions.R
@@ -16,6 +16,8 @@ r_estimate <- 0.74326344959  # Vasilev et al.'s estimate of the correlation betw
 thisYear <- 2023    # default for entering new data points
 first_shiny_meta_paper_full <- "Wolf, V., Kühnel, A., Teckentrup, V., Koenig, J., & Kroemer, N. B. (2021). Does transcutaneous auricular vagus nerve stimulation affect vagally mediated heart rate variability? A living and interactive Bayesian meta‐analysis. Psychophysiology, 58(11), e13933."
 first_shiny_meta_paper <- "Wolf, et al. (2021)"
+first_shiny_meta_paper_doi <- "https://doi.org/10.1111/psyp.13933"
+first_shiny_meta_paper_app <- "https://vinzentwolf.shinyapps.io/taVNSHRVmeta"
 noise_meta_paper_full <- "Vasilev, M. R., Kirkby, J. A., & Angele, B. (2018). Auditory distraction during reading: A Bayesian meta-analysis of a continuing controversy. Perspectives on Psychological Science, 13(5), 567-597."
 noise_meta_paper <- "Vasilev, et al. (2018)"
 
