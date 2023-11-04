388db7043c6b40260382487f6fd748dbfa759f70
David Allbritton <dallbrit@depaul.edu>
2023-11-04 15:35:09 -0700

    incremental backup

--- server.R
+++ server.R
@@ -598,10 +598,6 @@ server <- function(input, output, session) {
     MA <- merge(x = aggES, y = df_sub, by.x = "id", by.y = "ID") 
     MA <- unique(setDT(MA) [sort.list(id)], by = "id")
     MA <- with(MA, MA[order(MA$es)])
-#     ### debugging or temporary stuff:
-#     ###  store the new MA in a reactive values list, and save it to disk
-#     saveRDS(MA, file = paste0("MA", myrvs$ma_num, ".RDS"))
-# ###    myrv_MA[myrvs$ma_num] <- MA   ### debugging   
     myrvs$ma_num <- myrvs$ma_num + 1
     MA
   })
@@ -845,21 +841,6 @@ server <- function(input, output, session) {
                         MA.selected.data = req(as.data.frame(MA())), 
                         Source = Source 
                         )
-      ## for debugging; temporary.  copying bma to global environment
-      bma1 <<- bma()
-      saveRDS(bma(), file = "bma1.RDS")
-      MA1 <<- MA()
-      saveRDS(MA(), file = "MA1.RDS")
-      bc1 <<- Bayesmeta.Call
-      saveRDS(Bayesmeta.Call, file = "bc1.RDS")
-      ### that did not help.  would need a shinymeta expansion of Bayesmeta.Call ### debugging
-      
-      ###   store every MA and bma whenever bma is updated; 
-      ###   whenever MA is updated, check to see if it matches any of the stored MAs
-      ###   if it does, then copy the corresponding stored bma to bma and
-      ###   somehow prevent it from recalculating bma...  not sure how to do this last bit
-      ####
-      
       writexl::write_xlsx(sheetList, file)
     }
   )
--- tempscratchpad.R
+++ tempscratchpad.R
@@ -1,4 +1,85 @@
+# NOT THE REAL FILE!!!!!!!!!!!!!!!!!
+
+myrvs$previousModels <- data.frame()
+
+tempprevmods <- myrvs$previousModels   # debugging
+saveRDS(tempprevmods, file = "tempprevmods.RDS")   # debugging
+tempMA <- MA()        # debugging
+saveRDS(tempMA, file = "tempMA.RDS")   # debugging
+tempprinted_bma <- printed_bma()        # debugging
+saveRDS(tempprinted_bma, file = "tempprinted_bma.RDS")   # debugging
+
+prev <- readRDS("tempprevmods.RDS")
+ma1 <- readRDS("tempMA.RDS")
+bp1 <- readRDS("tempprinted_bma.RDS")
+ma1 <- readRDS("tempMA.RDS")
+oldbma <- readRDS("old_bma.RDS")
+
+
+identical(prev[[1]]$MA, prev[[2]]$MA)
+identical(prev[[1]]$printed_bma, prev[[2]]$printed_bma)
+
+identical(prev[[1]]$MA, ma1)
+identical(prev[[1]]$printed_bma, bp1)
+
+
+
+# for assigning new models, within the bma calculation block:
+#  myrvs$previousModels[1] <- list(MA = MA(), printed_bma = printed_bma(), bma = bma)
+
+
+
+    # get the data and bayesmeta parameters
+    MA <- MA()
+    printed_bma <- printed_bma()
+    # check to see if they are identical to any of the previously recorded ones
+    
+    # if they are, copy bma() from the stored one that goes with them and don't
+    # calculate bma() again.
+
+    # if they are not identical, then calculate bma() as usual
+    # and add the three things to the "previousbmas" list
+
+# function for checking old models
+    checkOldModels <- function(dataframewitholdmodels)
+    if (nrow(dataframewitholdmodels)) {
+      #check against all rows of myrvs$previousModels
+      # if match, return that bma
+      # else return false
+    }
+
+
+### for debugging
+##  maybe this should be a function, and it should be called in the bma() section
+##  instead of being a separate observe() of the recalculate button.
+# function: get_previous_bma (MA, printed_bma, previous_models)
+#   check whether any old ones are identical to both. if so, return old bma()
+#   if not, return FALSE
+# in the bma() section, do this:
+#   isNewOne <- get_previous_bma(MA(), printed_bma())
+#   if (isNewOne) bma <- isNewOne
+#   else {the rest of the usual stuff to assign a value to bma}
+#   bma
 #
+
+# create a shinymeta expansion of the reactive bma()
+printed_bma <- reactive(
+  expandChain(bma())
+)
+
+
+
+a <- list(x=c(1:5), y="asdfas")
+
+
+
+
+
+
+
+
+
+
 tempplot <- readRDS("robustplot1.RDS")
 tempplot <- readRDS("robustplot1_allstudies_default_priors.RDS")
 
