5aafeef05bf6b0f30066566386e62235559754c4
David Allbritton <davidallbritton@gmail.com>
2023-11-22 20:45:23 -0800

    increase file size max

--- server.R
+++ server.R
@@ -19,7 +19,7 @@ server <- function(input, output, session) {
   
   
   # increase the allowable file size for uploads:
-  options(shiny.maxRequestSize = 40 * 1024^2)
+  options(shiny.maxRequestSize = 100 * 1024^2)
   # shinyOptions(cache = cachem::cache_disk("./myapp-cache"))
   
   ## Initialize with stored data, which will be replaced when a data file is uploaded
